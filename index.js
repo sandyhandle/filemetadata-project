654a0e7dbaa658963867ada1ec47a225871a55c3
Santhosh Puvaneswaran <santhosh.puvaneswaran@gmail.com>
2022-10-18 17:14:46 +0530

    everything is up and working

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require("multer")
+var upload = multer({ dest: "uploads/"})
 var app = express();
 
 app.use(cors());
@@ -11,6 +12,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse",upload.single("upfile"), (req,res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size:req.file.size
+  })
+})
 
 
 
